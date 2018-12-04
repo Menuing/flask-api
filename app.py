983f472e92a7832ec84dc6660482c6de266dece9
marc.ribalta <marc.ribalta@eurecat.org>
2018-12-04 18:48:00 +0100

    defined the only method

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+# This method will return the probability of a user liking a recipe
+@app.route("/recipe_prob")
+def calculate_recipe():
+    # request.args.get('rating')
+    # request.args.get('list')
+    return "YEAH!"
 
 
 if __name__ == '__main__':
