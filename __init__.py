0024f8d603ba178918aff65212f2e68b9c1d81ae
marc.ribalta <marc.ribalta@eurecat.org>
2018-12-06 23:26:22 +0100

    flask api done

---
+++ __init__.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, Response
+from controller.RecipePredictionController import RecipePredictionController
+
+app = Flask(__name__)
+rpc = RecipePredictionController("logreg.joblib")
+
+# This method will return the probability of a user liking a recipe
+# /recipe_prob?rating=4.2&numIngUser=3&listIngRecipe=dried%20fruit&listIngRecipe=dill&listIngRecipe=dallas&listIngRecipe=curry
+@app.route("/recipe_prob")
+def calculate_recipe():
+    return rpc.predict(request)
+
+@app.route("/")
+def welcome():
+    return "Call /recipe_prob with params ?rating : The mean rate of a recipe, ?numIngUser : number of ingredients a user likes, ?listIngRecipe : for each ingredient a recipe has"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
